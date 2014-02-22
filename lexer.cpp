d10bdb656112dfd790dc719236ca71bb9f00ad71
R0mai <r0maikx02b@gmail.com>
2014-02-22 12:31:31 +0100

    Basic lexer main()

--- lexer.cpp
+++ lexer.cpp
@@ -1,4 +1,13 @@
 
-int main() {
-	
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <FlexLexer.h>
+#include <cstdlib>
+
+int main(int argc, char **argv) {
+    ifstream in(argv[1]);
+    yyFlexLexer fl(&in, &cout);
+    fl.yylex();
+    return 0;
 }
