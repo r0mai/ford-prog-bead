b18ab3b689f075ebc681b32b55381b602327a9d8
R0mai <r0maikx02b@gmail.com>
2014-02-22 12:44:44 +0100

    fixed includes and indentation

--- lexer.cpp
+++ lexer.cpp
@@ -1,13 +1,11 @@
 
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <FlexLexer.h>
-#include <cstdlib>
 
 int main(int argc, char **argv) {
-    ifstream in(argv[1]);
-    yyFlexLexer fl(&in, &cout);
-    fl.yylex();
-    return 0;
+	std::ifstream in(argv[1]);
+	yyFlexLexer fl(&in, &std::cout);
+ 	fl.yylex();
+	return 0;
 }
